4aa7f697b5b90a21a6d9b3075e1f309c79bff8fe
Nyeste Reka <rekabeka37@gmail.com>
2024-09-21 16:27:50 +0200

    Smoking time counter is updated and ready to use

--- app/src/main/java/com/example/molstop/MainActivity.java
+++ app/src/main/java/com/example/molstop/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.molstop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import java.util.Calendar;
@@ -15,11 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button cigiSzunetStart = findViewById(R.id.cigiStartBtn);
+        Button cigiSzunetEnd = findViewById(R.id.cigiEndBtn);
         Button beerkezesGomb = findViewById(R.id.beerkezesBtn);
+        TextView cStart = findViewById(R.id.cigiStart);
+        TextView cEnd = findViewById(R.id.cigiEnd);
         TextView beerkezes = findViewById(R.id.beerkezesDatum);
         TextView kilepes = findViewById(R.id.kilepesDatum);
         TextView maiDatum = findViewById(R.id.datum);
         Calendar calendar = Calendar.getInstance();
+
         int ev = calendar.get(Calendar.YEAR);
         int honap = calendar.get(Calendar.MONTH)+1;
         int nap = calendar.get(Calendar.DAY_OF_MONTH);
@@ -41,5 +45,20 @@ public class MainActivity extends AppCompatActivity {
             kilepes.setText(kilepesIdo);
         });
 
+        cigiSzunetStart.setOnClickListener(view -> {
+            Calendar calendar2 = Calendar.getInstance();
+            int cStartOra = calendar2.get(Calendar.HOUR);
+            int cStartPerc = calendar2.get(Calendar.MINUTE);
+            String cigiStartIdo = cStartOra + ":" + cStartPerc;
+            cStart.setText(cigiStartIdo);
+        });
+
+        cigiSzunetEnd.setOnClickListener(view -> {
+            Calendar calendar3 = Calendar.getInstance();
+            int cEndOra = calendar3.get(Calendar.HOUR);
+            int cEndPerc = calendar3.get(Calendar.MINUTE);
+            String cigiEndIdo = cEndOra + ":" + cEndPerc;
+            cEnd.setText(cigiEndIdo);
+        });
     }
 }
