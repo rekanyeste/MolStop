486b6c68a9cea2d239c09c14630cd61bd559ff5e
Nyeste Reka <rekabeka37@gmail.com>
2024-09-21 15:05:30 +0200

    Actual day is configured

--- app/src/main/java/com/example/molstop/MainActivity.java
+++ app/src/main/java/com/example/molstop/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.molstop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView maiDatum = findViewById(R.id.datum);
+        Calendar calendar = Calendar.getInstance();
+        int ev = calendar.get(Calendar.YEAR);
+        int honap = calendar.get(Calendar.MONTH)+1;
+        int nap = calendar.get(Calendar.DAY_OF_MONTH);
+        int ora = calendar.get(Calendar.HOUR);
+        int perc = calendar.get(Calendar.MINUTE);
+        int masodperc = calendar.get(Calendar.SECOND);
+
+        String aktualisIdo = ev + "." + honap + "." + nap + ". " + ora + ":" + perc + ":" + masodperc;
+        maiDatum.setText(aktualisIdo);
+
     }
 }
